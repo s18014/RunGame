a765b3d444941431161ff827a09c5581febaee59
s18014 <s18014@std.it-college.ac.jp>
2018-12-05 23:43:35 +0900

    Test push

---
+++ Assets/Scripts/Stage1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1 : MonoBehaviour {
+    public Rigidbody rigidbody;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Start () {
+        rigidbody.position = new Vector3(0, 0, 100f);
+        rigidbody.velocity = Vector3.back * 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
